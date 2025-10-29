9224841ed9f291ca6537beba5c8262c8233edd2b
Студент группы IS <is@sielom.local>
2025-10-29 09:53:10 +0500

    checked

--- gagilev_prj/django_app/urls.py
+++ gagilev_prj/django_app/urls.py
@@ -3,6 +3,6 @@ from django.contrib.sitemaps.views import sitemap
 from .views import page1, page2
 
 urlpatterns = [
-    path('', page1),
-    path('Subscription', page2)
+    path('', page1, name=''),
+    path('Subscription', page2, name='subscription')
 ]
