dcb382c7021339ed3d428b843e97eb1c79a70154
Студент группы IS <is@sielom.local>
2025-11-18 11:09:09 +0500

    trenera

--- gagilev_prj/django_app/urls.py
+++ gagilev_prj/django_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.sitemaps.views import sitemap
-from .views import page1, page2, page3, page4
+from .views import page1, page2, page3, page4, trainer_search, trainer_detail
 
 urlpatterns = [
     path('', page1, name=''),
     path('Subscription', page2, name='subscription'),
     path('Staff', page3, name='staff'),
-    path('Timetable', page4, name='timetable')
+    path('Timetable', page4, name='timetable'),
+    path('trainer/<int:trainer_id>/', trainer_detail, name='trainer_detail'),
+    path('search/', trainer_search, name='trainer_search'),
 ]
--- gagilev_prj/django_app/views.py
+++ gagilev_prj/django_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404, redirect
 from .models import *
+from django.db.models import Q
 
 def page1(request):
     return render(request, 'page1.html')
@@ -21,3 +22,31 @@ def page3(request):
 
 def page4(request):
     return render(request, 'page4.html')
+
+def trainer_detail(request, trainer_id):
+    trainer = get_object_or_404(Trainer, id=trainer_id)
+    trainings = Training.objects.filter(trainer_id=trainer)
+    clients = Client.objects.filter(training__trainer_id=trainer).distinct()
+    
+    return render(request, 'search.html', {
+        'trainer': trainer,
+        'trainings': trainings,
+        'clients': clients,
+    })
+
+def trainer_search(request):
+    query = request.GET.get('q', '').strip()
+    
+    if query:
+        # Ищем всех тренеров по запросу
+        trainers = Trainer.objects.filter(
+            Q(first_name__icontains=query) |
+            Q(last_name__icontains=query) |
+            Q(father_name__icontains=query)
+        )
+        
+        return render(request, 'trainer_search_results.html', {
+            'trainers': trainers,
+            'query': query,
+            'results_count': trainers.count()
+        })
